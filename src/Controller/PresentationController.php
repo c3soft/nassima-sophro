66ec40a98adeaa7dedd6ca0aee7a5ffdb0f164d7
Pascal <fedodev55@gmail.com>
2021-03-23 16:15:31 +0100

    Add page presentation

---
+++ src/Controller/PresentationController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PresentationController extends AbstractController
+{
+    #[Route('/presentation', name: 'app_presentation')]
+    public function index(): Response
+    {
+        return $this->render('presentation/show.html.twig');
+    }
+}
