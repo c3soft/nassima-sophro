cc33a683c436de9b8691d9182c740db7781e56e8
Pascal <fedodev55@gmail.com>
2021-04-10 23:40:31 +0200

    Add favicons

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,14 @@
+// Loader 
+// ---------------------------------------------------------------------------
+window.onload = function () {
+    window.setTimeout(fadeout, 500);
+}
+
+function fadeout() {
+    document.querySelector('.preloader').style.opacity = '0';
+    document.querySelector('.preloader').style.display= 'none';
+}
+
 // Navbar personnalisé responsive
 // ---------------------------------------------------------------------------
 
